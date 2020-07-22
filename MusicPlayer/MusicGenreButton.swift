5ac6f29143463d3a35c3c0f5157008a9cd6da2da
Getnet12 <getnetmekuriyaw@gmail.com>
2020-07-22 10:41:08 -0500

    Initial commit

---
+++ MusicPlayer/MusicGenreButton.swift
@@ -0,0 +1,28 @@
+//
+//  MusicGenreButton.swift
+//  MusicPlayer
+//
+//  Created by Getnet Mekuriyaw on 7/21/20.
+//  Copyright © 2020 Getnet Mekuriyaw. All rights reserved.
+//
+
+import UIKit
+
+class MusicGenreButton: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func GenreButton(_ sender: UIButton) {
+    }
+    
+
+    @IBAction func StopButton(_ sender: UIButton) {
+    }
+    
+
+    @IBAction func NextButton(_ sender: UIButton) {
+    }
+}
